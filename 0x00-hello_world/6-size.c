f59ff686e56720e2a8b8011d0a64bb5c9e36dc54
prophet852 <dingawillie@outlook.com>
2022-04-12 00:20:11 +0200

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,10 @@
  */
 int main(void)
 {
-printf("Size of a char: %d bytes(s)\n", sizeof(char));
-printf("Size of a char: %d bytes(s)\n", sizeof(char));[D[int[[[C[C[));[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[int[[C[3~[3~[3~[3~[3~[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[Cf an int: %d byte(s)\yte(s)\n", sizeof(char);[B
-printf("Size of a long int: %d byte(s)\n", sizeof(long long int));
-printf(Size of a long long int: %d byte(s)\n",sizeof(long long int));[A[D[D[D[D[D[[C[C[C[C[C[C[C[C[C[C[long int));[B[B[C[C[C[C[C[C[C[C
+printf("Size of a char: %d byte(s)\n", sizeof(char);
+printf("Size of an int: %d byte(s)\n", sizeof(int));
+printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+printf(Size of a long long int: %d byte(s)\n",sizeof(long long int));
 printf("Size of a float: %d byte(s)\n", sizeof(float);
 return (0);
 }
