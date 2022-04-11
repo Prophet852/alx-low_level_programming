189213aa086553d8ee748583eaa62c85965ca937
prophet852 <dingawillie@outlook.com>
2022-04-12 00:16:56 +0200

    Size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *
+ *main - entry point
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("Size of a char: %d bytes(s)\n", sizeof(char));
+printf("Size of a char: %d bytes(s)\n", sizeof(char));[D[int[[[C[C[));[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[int[[C[3~[3~[3~[3~[3~[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[Cf an int: %d byte(s)\yte(s)\n", sizeof(char);[B
+printf("Size of a long int: %d byte(s)\n", sizeof(long long int));
+printf(Size of a long long int: %d byte(s)\n",sizeof(long long int));[A[D[D[D[D[D[[C[C[C[C[C[C[C[C[C[C[long int));[B[B[C[C[C[C[C[C[C[C
+printf("Size of a float: %d byte(s)\n", sizeof(float);
+return (0);
+}
