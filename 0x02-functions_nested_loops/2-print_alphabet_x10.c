beca00cc6a8c5a4163ffd6ec39326f511f6efaf7
prophet852 <dingawillie@outlook.com>
2022-04-12 20:34:06 +0200

    Prints 10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,14 +3,14 @@
 /**
   * print_alphabet_x10 - Make alphabet x10 times
   *
-  * Return: x10 a-z
+  * Return: void
   */
 void print_alphabet_x10(void)
 {
 	char c;
 	int i = 0;
 
-	while (i <= 10)
+	while (i <= 9)
 	{
 		for (c = 'a'; c <= 'z'; c++)
 		{
