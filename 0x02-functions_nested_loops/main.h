ddaf503bc0f077e9bcbf6311688b7e24cdc870a0
prophet852 <dingawillie@outlook.com>
2022-04-13 18:10:21 +0200

    My main h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,9 +1,9 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef _MAIN_H_INCLUDED
+#define _MAIN_H_INCLUDED
 
 int _putchar(char c);
-int print_alphabet(void);
-int print_alphabet_x10(void);
+void print_alphabet(void);
+void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
@@ -14,4 +14,5 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
-#endif /* _MAIN_H_ */
+
+#endif
