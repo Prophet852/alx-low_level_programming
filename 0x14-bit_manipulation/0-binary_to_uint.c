7010f6c645dbed6871d20a66346779e72a1a2a9d
Prophet852 <dingawillie@outlook.com>
2022-05-28 11:17:10 -0700

    uint

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -5,14 +5,15 @@
 /**
  *  binary_to_uint - Converts a binary number to an unsigned int
  *  @b: The binary string to converts
+ *
  *  Return: The positive number converted from a binary
  */
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int len = 0, count = 0, sum = 0;
 
-       	if (b == NULL)
-        	return (0);
+	if (b == NULL)
+		return (0);
 
 	len = _strlen(b);
 	while (len--)
