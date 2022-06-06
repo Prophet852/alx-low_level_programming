1145516e7b7907d7ba34a41e2c9e63f414e48ef0
Prophet852 <dingawillie@outlook.com>
2022-06-06 09:00:37 -0700

    strpbrk

--- 0x09-static_libraries/4-strpbrk.c
+++ 0x09-static_libraries/4-strpbrk.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "main.h"
 
 /**
@@ -21,50 +20,4 @@ char *_strpbrk(char *s, char *accept)
 		}
 	}
 	return ('\0');
-=======
-nclude "main.h"
-
-
-
-/**
- *
- *  * _strpbrk - searches a string for any of a set of bytes.
- *
- *   * @s: first string.
- *
- *    * @accept: second string.
- *
- *     * Return: a pointer to the byte in s that matches one of the
- *
- *      * bytes in accept, or NULL if no such byte is found.
- *
- *       */
-
-char *_strpbrk(char *s, char *accept)
-
-{
-
-		unsigned int i, j;
-
-
-
-			for (i = 0; *(s + i) != '\0'; i++)
-
-					{
-
-								for (j = 0; *(accept + j) != '\0'; j++)
-
-											{
-
-															if (*(s + i) == *(accept + j))
-
-																				return (s + i);
-
-																	}
-
-									}
-
-				return ('\0');
-
->>>>>>> ac94d57b6d6f02fc2984d0977541d344ee13b834
 }
